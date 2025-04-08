bef665efb25be9c02f5bab32072ff776c8d680e5
jackman120 <94422263+jackman120@users.noreply.github.com>
2025-04-08 14:55:30 -0400

    Added assets

--- Assets/Moving.cs
+++ Assets/Moving.cs
@@ -7,8 +7,8 @@ public class Example : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 500.0f;
-    private float jumpHeight = 1.0f;
+    private float playerSpeed = 3.0f;
+    private float jumpHeight = 5.0f;
     private float gravityValue = -9.81f;
 
     private void Start()
